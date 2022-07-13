1ddef0d85237f2c57a1232eda7367ea7c9b2504f
clement.lafon <clement.lafon@comwatt.com>
2022-07-13 15:55:22 +0200

    Use different loop for of instead of for loop, tidy code

--- js/product.js
+++ js/product.js
@@ -8,46 +8,41 @@ function getProduct() {
     })
 }
 
+function addData(product) {
+    
+    //Sélectionne la div qui à pour classe item__img.
+    let displayImg = document.getElementsByClassName('item__img')[0];
+    displayImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.description}">`
+    //Sélectionne la div qui à pour ID title.
+    let displayTitle = document.getElementById('title');
+    displayTitle.innerText = `${product.name}`;
+    //Sélectionne la div qui à pour ID price.
+    let displayPrice = document.getElementById('price');
+    displayPrice.innerText = `${product.price}`;
+    //Sélectionne la div qui à pour ID description.
+    let displayDescription = document.getElementById('description');
+    displayDescription.innerText = `${product.description}`;
+    //Sélectionne la div qui à pour ID colors.
+    let displayColors = document.getElementById('colors');
+    
+    for(let color of product.colors) {
+        var opt = document.createElement("option");
+        opt.value = color;
+        opt.text = color;
+        displayColors.add(opt, null);
+    }
+}
+
 //Récupération de l'id de l'url.
 var realTimeUrl = window.location.href;
-
 var url = new URL(realTimeUrl);
-
 var search_params = new URLSearchParams(url.search);
 
 if(search_params.has('id')) {
     var id = search_params.get('id');
 }
 
-//Sélectionne la div qui à pour classe item__img.
-let displayImg = document.getElementsByClassName('item__img')[0];
-//Sélectionne la div qui à pour ID title.
-let displayTitle = document.getElementById('title');
-//Sélectionne la div qui à pour ID price.
-let displayPrice = document.getElementById('price');
-//Sélectionne la div qui à pour ID description.
-let displayDescription = document.getElementById('description');
-//Sélectionne la div qui à pour ID colors.
-let displayColors = document.getElementById('colors');
-
 //Fonction qui récupère les datas et les affiche pour chaque produits.
 getProduct().then(product => {
-
-    displayImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.description}">`
-
-    displayTitle.innerHTML = `${product.name}`;
-
-    displayPrice.innerHTML = `${product.price}`;
-
-    displayDescription.innerHTML = `${product.description}`;
-
-    let numberOfColors = product.colors.length;
-
-    for(let colors = 0; colors < numberOfColors; colors++) {
-        var opt = document.createElement("option");
-        opt.value = product.colors[colors];
-        opt.text = product.colors[colors];
-        displayColors.add(opt, null);
-    }
-
+    addData(product);
 }).catch(err => console.log(err));
