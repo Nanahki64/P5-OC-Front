b3564902cf88f14e8b9c21a1db17dd8cc3a1b732
Nanahki64 <ukkileipod@gmail.com>
2022-07-13 14:50:06 +0200

    Changer way to bind datas and ADD colors options

--- js/product.js
+++ js/product.js
@@ -19,37 +19,6 @@ if(search_params.has('id')) {
     var id = search_params.get('id');
 }
 
-//Récupère le template pour la classe item__img.
-function getProductTemplateImg(product) {
-    return `<img src="${product.imageUrl}" alt="${product.description}">`
-}
-
-//Récupère le template pour l'ID title.
-function getProductTemplateTitle(product) {
-    return `${product.name}`
-}
-
-//Récupère le template pour l'ID price.
-function getProductTemplatePrice(product) {
-    return `${product.price}`
-}
-
-//Récupère le template pour l'ID description.
-function getProductTemplateDescription(product) {
-    return `${product.description}`
-}
-
-//Récupère le template pour l'ID colors.
-function getProductTemplateColors(product) {
-    let arrayColors = product.colors;
-    console.log(arrayColors);
-    
-    arrayColors.forEach(element => {
-        console.log(element);
-        //return `<option value="${product.colors}">${product.colors}</option>`
-    });
-}
-
 //Sélectionne la div qui à pour classe item__img.
 let displayImg = document.getElementsByClassName('item__img')[0];
 //Sélectionne la div qui à pour ID title.
@@ -59,30 +28,26 @@ let displayPrice = document.getElementById('price');
 //Sélectionne la div qui à pour ID description.
 let displayDescription = document.getElementById('description');
 //Sélectionne la div qui à pour ID colors.
-let displayColors = document.getElementsByTagName('option');
+let displayColors = document.getElementById('colors');
 
 //Fonction qui récupère les datas et les affiche pour chaque produits.
 getProduct().then(product => {
-    let htmlImage = "";
-    let image = htmlImage += getProductTemplateImg(product);
-    displayImg.innerHTML = image;
 
-    let htmlName = "";
-    let name = htmlName += getProductTemplateTitle(product);
-    displayTitle.innerHTML = name;
+    displayImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.description}">`
 
-    let htmlPrice = "";
-    let price = htmlPrice += getProductTemplatePrice(product);
-    displayPrice.innerHTML = price;
+    displayTitle.innerHTML = `${product.name}`;
 
-    let htmlDescription = "";
-    let description = htmlDescription += getProductTemplateDescription(product);
-    displayDescription.innerHTML = description;
+    displayPrice.innerHTML = `${product.price}`;
 
-    let htmlColors = "";
-    let colors = htmlColors += getProductTemplateColors(product);
-    displayColors = colors;
+    displayDescription.innerHTML = `${product.description}`;
 
-    console.log(product);
+    let numberOfColors = product.colors.length;
+
+    for(let colors = 0; colors < numberOfColors; colors++) {
+        var opt = document.createElement("option");
+        opt.value = product.colors[colors];
+        opt.text = product.colors[colors];
+        displayColors.add(opt, null);
+    }
 
 }).catch(err => console.log(err));
